9bcfaba7a710efdb1d94af250b89a4462c79f4f5
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2024-04-24 12:39:56 +0530

    response, error handler & user, video model

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -5,4 +6,14 @@ dotenv.config({
   path: "../.env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGO db connection failed:", err);
+  });
